9fc81cc5062b084210977eacf6d3ecc703c35f35
Angular CLI <angular-cli@angular.io>
2017-12-20 13:35:32 -0200

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'apu-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'apu';
+}
