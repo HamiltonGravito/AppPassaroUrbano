5bcfa341e07d0121cd936eb0cc768118c791bff7
HamiltonGravito <hamiltongravito@gmail.com>
2018-01-19 20:29:51 -0200

    Validação dos campos do formulário 'front-end' e criação do modelo Pedido.

---
+++ src/app/ordem-compra.service.ts
@@ -0,0 +1,7 @@
+import { Pedido } from '../app/shared/pedido.model'
+
+export class OrdemCompraService {
+    public efetivarCompra(pedido: Pedido){
+        console.log('Ordem de compra service');
+    }
+}
--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+//Serviço - 1ºPasso: Inserir classe de Serviço
+import { OrdemCompraService } from '../ordem-compra.service'
+
 @Component({
   selector: 'apu-ordem-compra',
   templateUrl: './ordem-compra.component.html',
-  styleUrls: ['./ordem-compra.component.css']
+  styleUrls: ['./ordem-compra.component.css'],
+  //Serviço - 2ºPasso: Prover o Serviço
+  providers: [OrdemCompraService]
 })
 export class OrdemCompraComponent implements OnInit {
 
@@ -12,54 +17,87 @@ export class OrdemCompraComponent implements OnInit {
   public complemento: string = null;
   public formaPagamento: string = null;
 
+  public formEstado: string = "disabled";
+
   //Atributos de controle de validação dos campos
   public enderecoValido: boolean;
   public numeroValido: boolean;
   public complementoValido: boolean;
   public formaPagamentoValida: boolean;
 
-  constructor() { }
+  //Atributos de controle de estado primitivo dos campos (pristine)
+  public enderecoEstadoPrimitivo: boolean = true;;
+  public numeroEstadoPrimitivo: boolean = true;
+  public complementoEstadoPrimitivo: boolean = true;
+  public formaPagamentoEstadoPrimitivo: boolean = true;
+
+  //Serviço - 3ºPasso: Injetar o serviço a partir do construtor
+  constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
+    //this.ordemCompraService.efetivarCompra();
   }
 
   public atualizaEndereco(endereco: string) : void {
     this.endereco = endereco;
+    this.enderecoEstadoPrimitivo = false;
     //validação do endereco
     if(this.endereco.length > 4 && this.endereco.includes("Rua") || this.endereco.includes("Avenida") || this.endereco.includes("Travessa") || this.endereco.includes("Praça")){
       this.enderecoValido = true;
     } else {
       this.enderecoValido = false;
     }
+
+    this.habilitaForm();
   }
 
   public atualizaNumero(numero: number) : void {
     this.numero = numero;
+    this.numeroEstadoPrimitivo = false;
     //validação do numero - Is Not a Number, ou seja, false se for uma sequencia de numeros e verdadeiros se for string
-    if(isNaN(this.numero)){
-      this.numeroValido = false;
-    } else {
+    if(!isNaN(this.numero) && this.numero > 0){
       this.numeroValido = true;
+    } else {
+      this.numeroValido = false;
     }
+
+    this.habilitaForm();
   }
 
   public atualizaComplemento(complemento: string) : void {
     this.complemento = complemento;
+    this.complementoEstadoPrimitivo = false;
     //validação complemento
     if(this.complemento.length > 0){
       this.complementoValido = true;
     } else {
       this.complementoValido = false;
     }
+
+    this.habilitaForm();
   }
 
   public atualizaFormaPagamento(formaPagamento: string) : void {
     this.formaPagamento = formaPagamento;
+    this.formaPagamentoEstadoPrimitivo = false;
     //validação formaPagamento
     if(this.formaPagamento !== ""){
       this.formaPagamentoValida = true;
     } else {
       this.formaPagamentoValida = false;
+    }
+
+    this.habilitaForm();
+  }
+
+  public habilitaForm(): void {
+    if( this.enderecoValido === true &&
+        this.numeroValido === true &&
+        this.formaPagamentoValida === true
+    ){
+      this.formEstado = '';
+    } else {
+      this.formEstado = 'disabled';
     }
   }
 
